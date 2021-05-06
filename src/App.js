0474922aa5d828fec892335830260b4cbc30a442
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-05-06 14:24:34 -0700

    feat(axios-fetch): fetched data with axios and saved data to state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
 
+  const [books, setBooks] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://www.anapioficeandfire.com/api/books"
+      );
+      console.log(response.data);
+      setBooks(response.data);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="container my-5">
       <h2 className="display-4 text-center">Game of Thrones Books</h2>
+      <p>You have {books.length} to sell.</p>
     </div>
   );
 }
