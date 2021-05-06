0e3f95de1569720ad82c615f15df09e49ac30aff
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-05-06 16:21:37 -0700

    feat(book-card): created card component for book to be rendered onto app.js

---
+++ src/components/BookCard.js
@@ -0,0 +1,49 @@
+const BookCard = ( { books } ) => {
+
+    return (
+        <div className="row">
+            {books.map( (book, index) => {
+                return (
+                    <div className ="card d-flex col-4" key={index}>
+                        <div className="card-body">
+                            <div className="card-title display-6">
+                                <strong>Title:</strong>{' '}{book.name}
+                            </div>
+                            
+                            <div className="card-text">
+                                <strong>ISBN:</strong>{' '}{book.isbn}
+                            </div>
+
+                            <div className="className">
+                                    <strong>Author(s):</strong>{' '}
+                                    {book.authors.map( (author, index) => {
+                                        return (
+                                            <div key={index}>
+                                                {author}
+                                            </div>
+                                        );
+                                    })}
+                            </div>
+                            
+                            <div className="card-text">
+                                <strong>Number of Pages:</strong>{' '}{book.numberOfPages}
+                            </div>
+                            
+                            <div className="card-text">
+                                <strong>Country:</strong>{' '}{book.country}
+                            </div>
+
+                            <div className="card-text">
+                                <strong>Released date:</strong>{' '}{book.released.slice(0,10)}
+                            </div>
+
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+
+}
+
+export default BookCard;
